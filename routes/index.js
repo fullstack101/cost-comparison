7e2ceffed742af064e95dcb512357cb18c8a6199
Dimitar Kumanov <dimitar_kumanov@yahoo.com>
2017-02-16 16:01:56 +0200

    Chained API calls through promises.

--- routes/index.js
+++ routes/index.js
@@ -1,66 +1,63 @@
-var express = require('express');
-var router = express.Router();
-var http = require('http');
-var request = require('request');
+const express = require('express');
+const router = express.Router();
+const http = require('http');
+const request = require('request');
 const getip = require('client-ip');
 const fs = require('fs');
 const path = require('path');
 const jsonParser=require('json-parser');
+const fetch = require('node-fetch');
 
 
-/* GET home page. */
-// way 1 using request
-var options = {
-    method: 'get',
-    json: true,
-    url: "https://www.numbeo.com/api/city_prices?api_key=<API_Key>&query=Belgrade"
+const getCity = function (userIP){
+    return new Promise(function(resolve, reject) {
+        fetch('https://freegeoip.net/json/100.105.14.101')
+            .then(function(res) {
+                return res.json();
+            }).then(function(localeJSON) {
+            return localeJSON.city;
+        }).then(function(city){
+            city=(city == "" ? "Belgrade" : city);
+            fetch("https://www.numbeo.com/api/city_prices?api_key=<API_Key>&query="+city)
+                .then(function(res){
+                    return res.json();
+                }).then(function(cityStatsJSON) {
+                resolve(cityStatsJSON);
+            });
+        })
+    });
 };
 
-// way 2 using Promises
-const getContent = function(url) {
-    // return new pending promise
-    return new Promise((resolve, reject) => {
-            // select http or https module, depending on reqested url
-            const lib = url.startsWith('https') ? require('https') : require('http');
-    const request = lib.get(url, (response) => {
-            // handle http errors
-            if (response.statusCode < 200 || response.statusCode > 299) {
-        reject(new Error('Failed to load page, status code: ' + response.statusCode));
-    }
-    // temporary data holder
-    const body = [];
-    // on every content chunk, push it to the data array
-    response.on('data', (chunk) => body.push(chunk));
-    // we are done, resolve promise with those joined chunks
-    response.on('end', () => resolve(body.join('')));
-});
-    // handle connection errors of the request
-    request.on('error', (err) => reject(err))
-})
-};
 router.get('/numbeo', function (req, res, next) {
-    // way 1 using request
-    // request(options, function (err, res, body){console.log(body);
-    //     });
-    // way 2 using Promises
-    getContent('https://www.numbeo.com/api/city_prices?api_key=0ifio49inu2plm&query=Belgrade')
-        .then((html) => console.log(jsonParser.parse(html)))
-    .catch((err) => console.error(err));
-
-    res.render('index', {"city": 'Belgrade', "title": "Numbeo"});
+    let ip = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+    ip = ip.substring(7);
+    getCity(ip).then(function (cityStats) { res.end(JSON.stringify(cityStats)); });
 });
-router.get('/test?:id', function (req, res, next) {
 
+
+
+router.get('/testIP', function (req, res, next) {
     console.log("second");
     //res.locals.city = req.query.id;
-    var ip = req.ip;
-    res.render('index', {"city": ip, "title": req.query.id});
+    let ip = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+    res.render('index', {title: 'IP', text: ip});
+});
+
+router.get('/arguments?:city', function (req, res, next) {
+    console.log("first");
+    let city = req.query.city;
+    res.render('index', {title: 'Arguments', text: city});
 });
 
 router.get('/', function (req, res, next) {
     console.log("first");
-    res.render('index', {title: 'Express'});
+    res.render('index', {title: 'Express', text: "Used for testing"});
 });
-
 
 module.exports = router;
